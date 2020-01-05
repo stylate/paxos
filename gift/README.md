528c848c46f17a10bdecad8de8c0b9ad33c59b6e
Alan Nguyen <nguyenalan@berkeley.edu>
2020-01-04 21:04:34 -0800

    first runthrough of q2

---
+++ gift/README.md
@@ -0,0 +1,2 @@
+ideas:
+    - 0-1 knapsack, use complements to figure out cheapest gifts 
---
+++ gift/solution.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def find_gifts(prices, balance):
+    '''
+    Finds two distinct gifts given PRICES whose sum is minimally 
+    under (or equal to) the gift card balance.
+    '''
+    left, right = 0, len(prices) - 1
+    optimal = []
+    min_diff = float('inf')
+    while left < right:
+        sum_prices = prices[left][1] + prices[right][1]
+        if sum_prices < balance:
+            leftover = balance - sum_prices # how much funds are leftover. guaranteed to be > 0
+            if leftover > min_diff:
+                min_diff = leftover
+                optimal = [prices[left], prices[right]]
+            left += 1
+        elif sum_prices == balance:
+            return [prices[left], prices[right]]
+        else:
+            right -= 1
+    return optimal
+
+if __name__ == '__main__':
+    # take in inputs
+    if len(sys.argv) != 3:
+        print("Invalid user input. Number of arguments isn't equal to 2 or the user inputs themselves are invalid.")
+    else:
+        text, balance = sys.argv[1], sys.argv[2]
+        # validate/process args
+        fp = open(text)
+        prices = []
+        for line in fp:
+            gift, price = line.strip().split(', ')
+            prices.append((gift, int(price))) 
+        print("prices: ", prices)
+        # print optimal gifts in stdout
+        gifts = find_gifts(prices, int(balance))
+        if gifts:
+            item1, item2 = gifts[0], gifts[1]
+            output = item1[0] + " " + str(item1[1]) + ", " + item2[0] + " " + str(item2[1])
+        else:
+            output = "Not possible"
+        print(output)
